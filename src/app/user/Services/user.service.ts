3582b4ae1e9cf4e9f55069f6e6a2f8c0ff44c011
Tania Garcia Font <tgarciafo@uoc.edu>
2021-10-18 08:17:30 +0200

    suprimir missatges

--- src/app/user/Services/user.service.ts
+++ src/app/user/Services/user.service.ts
@@ -36,34 +36,19 @@ export class UserService {
       );      
   }
 
-  /**
-   * Función que devuelve un usuario.
-   * @param id - Identificador del usuario
-   * @return Observable<User>
-   */
+
   getUser(id: number): Observable<User> {
-/*     const url = `${this.API_ENDPOINT}/getUser/${id}`;
- */    return this.http.get<User>(this.API_ENDPOINT + '/getUser/'+id).pipe(
+   return this.http.get<User>(this.API_ENDPOINT + '/getUser/'+id).pipe(
       catchError(this.handleError<User>(`getUser id=${id}`))
     );
   }
 
-  /**
-   * Función que actualiza un usuario.
-   * @param user - usuario a actualizar
-   * @return Observable<User>
-   */
   updateUser(user: User): Observable<User> {
     return this.http.put<User>(this.API_ENDPOINT, user, this.httpOptions).pipe(
       catchError(this.handleError<any>('updateUser'))
     );
   }
 
-  /**
-   * Función que comprueba si existe un usuario.
-   * @param user - usuario a añadir
-   * @return Observable<boolean>
-   */
   userExist(user: User): Observable<boolean> {
     return this.http.get<User[]>(this.API_ENDPOINT + '/users').pipe(
       map((users) => {
@@ -79,11 +64,6 @@ export class UserService {
     );
   }
 
-  /**
-   * Función que añade un usuario.
-   * @param user - usuario a añadir
-   * @return Observable<User>
-   */
   addUser(user: User): Observable<any> {
     return this.userExist(user).pipe(
       exhaustMap((exist) => {
@@ -99,19 +79,11 @@ export class UserService {
     );
   }
 
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
+      console.error(error); 
 
-      // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
