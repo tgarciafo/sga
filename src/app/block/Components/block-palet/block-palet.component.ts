ba4cebbf093824d0c9c6b55e604715b6da8e2d5e
Tania Garcia Font <tgarciafo@uoc.edu>
2021-11-04 20:04:48 +0100

    fent bloquejar

--- src/app/block/Components/block-palet/block-palet.component.ts
+++ src/app/block/Components/block-palet/block-palet.component.ts
@@ -1,4 +1,10 @@
+declare var parseBarcode: any;
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
+import { createBlock, getAllBlocks } from '../../actions';
+import { BlockState } from '../../reducers';
 
 @Component({
   selector: 'app-block-palet',
@@ -7,9 +13,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlockPaletComponent implements OnInit {
 
-  constructor() { }
+  public sscc: FormControl;
+  public blockForm: FormGroup;
+  public errorBlock: any;
+  public bSubmitted: boolean;
+
+  blockState$: BlockState;
+
+  constructor(private formBuilder: FormBuilder, private store: Store<AppState>) {
+    this.store.select('blockApp').subscribe(block => this.blockState$ = block);
+  }
 
   ngOnInit(): void {
+    this.store.dispatch(getAllBlocks());
+
+    this.bSubmitted = false;
+    this.sscc = new FormControl('', [Validators.required]);
+    this.errorBlock = '';
+
+    this.blockForm = this.formBuilder.group({
+      sscc: this.sscc,
+    });
   }
+
+  saveBlock(){
+
+    this.bSubmitted = true;
+
+    /* const form = this.blockForm.value;
+
+    this.store.dispatch(createBlock({block:form})); */
+
+    this.blockForm.reset();
+
+  }
+
+  interpreteBarcode(){
+    "use strict";
+
+    try{
+
+      let answer= parseBarcode(this.codi());
+
+      return answer.parsedCodeItems.forEach(this.basedades);
+
+    } catch (e){
+      console.log(e);
+  }
+    
+  }
+
+  basedades(element: any, index: any,array: any){
+    const ai=element.ai;
+    let data=element.data;
+
+    if (ai=='00'){
+      this.sscc.setValue=(data);
+    } 
+  }  
+
+  codi(){
+    if (this.sscc.value != ''){
+      return this.sscc.value;
+    }
+  }  
 
 }
--- src/app/block/models/block.ts
+++ src/app/block/models/block.ts
@@ -1,4 +1,7 @@
 export interface Block {
     block_id?:number;
     sscc: number;
+    description_client: string;
+    description_prod: string;
+    caducitat: Date;
 }
